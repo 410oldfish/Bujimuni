0647e0f00e91ee993ea2020151212b236ae01d44
yhy <472303831@qq.com>
2024-01-03 00:24:32 +0800

    [feat]角色移动

--- Bujimuni/Assets/Scripts/Game/Player.Designer.cs
+++ Bujimuni/Assets/Scripts/Game/Player.Designer.cs
@@ -1,10 +1,12 @@
-// Generate Id:e530099a-803a-4302-bac0-6fc8b38bcd8c
+// Generate Id:7a717a0e-0da8-44a1-876c-5522cd4111a1
 using UnityEngine;
 
 namespace Bujimuni
 {
 	public partial class Player
 	{
+
+		public SpriteRenderer Square;
 
 	}
 }
--- Bujimuni/Assets/Scripts/Game/Player.cs
+++ Bujimuni/Assets/Scripts/Game/Player.cs
@@ -1,13 +1,49 @@
+using System;
 using UnityEngine;
 using QFramework;
+using Unity.Mathematics;
 
 namespace Bujimuni
 {
 	public partial class Player : ViewController
 	{
+		private Sprite UpSprite;
+		private Sprite DownSprite;
+		private Sprite LeftSprite;
 		void Start()
 		{
 			"Hello Bujimuni!".LogInfo();
+			var bujiSprite = Resources.LoadAll<Sprite>("Texture/buji");
+			UpSprite = bujiSprite[2];
+			DownSprite = bujiSprite[0];
+			LeftSprite = bujiSprite[1];
+		}
+
+		private void Update()
+		{
+			//Move
+			if (Input.GetKey(KeyCode.W))
+			{
+				transform.position += transform.up * Time.deltaTime * 10;
+				Square.sprite = UpSprite;
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				transform.position -= transform.up * Time.deltaTime * 10;
+				Square.sprite = DownSprite;
+			}
+			if (Input.GetKey(KeyCode.A))
+			{
+				transform.position -= transform.right * Time.deltaTime * 10;
+				Square.sprite = LeftSprite;
+				transform.localScale = new Vector3(math.abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				transform.position += transform.right * Time.deltaTime * 10;
+				Square.sprite = LeftSprite;
+				transform.localScale = new Vector3(-math.abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+			}
 		}
 	}
 }
